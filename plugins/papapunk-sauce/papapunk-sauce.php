bcb09c31f7ad812dde9fc28ff51ee4b84aed99a9
frank0000 <frank_oh@hotmail.com>
2016-07-11 15:12:50 -0700

    inital Punk Tags taxonomy

--- plugins/papapunk-sauce/papapunk-sauce.php
+++ plugins/papapunk-sauce/papapunk-sauce.php
@@ -27,5 +27,44 @@
 
     // removes the WordPress version from your header for security
     add_filter( 'the_generator', '__return_null' );
-    
-?>l
+
+
+    add_action( 'init', 'register_papapunk_taxonomies' );
+    function register_papapunk_taxonomies() {
+        register_punk_tag_taxonomies();
+    }
+
+    function register_punk_tag_taxonomies() {
+
+        register_taxonomy(
+            'punk-tag',
+            array( 'post', 'page' ),
+            array(
+                'labels' => array(
+                    'name' => __('Punk Tags'),
+                    'singular_name' => __('Punk Tag'),
+                    'all_items' => __( 'All Punk Tags' ),
+                    'edit_item' => __( 'Edit Punk Tag' ),
+                    'view_item' => __( 'View Punk Tag' ),
+                    'update_item' => __( 'Update Punk Tag' ),
+                    'add_new_item' => __( 'Add New Punk Tag' ),
+                    'new_item_name' => __('New Punk Tag Name'),
+                    'parent_item' => ('Parent Punk Tag'),
+                    'parent_item_colon' => ('Parent Punk Tag:'),
+                    'search_items' => __( 'Search Punk Tags' ),
+                    'popular_items' => __( 'Popular Punk Tags' ),
+                    'separate_items_with_commas' => __( 'Separate Punk Tags with commas' ),
+                    'add_or_remove_items' => __( 'Add or remove Punk Tags' ),
+                    'choose_from_most_used' => __( 'Choose from the most used Punk Tags' ),
+                    'not_found' => __( 'No Punk Tags found' ),
+                ),
+                'hierarchical' => true,
+                'show_ui' => true,
+                'query_var' => true,
+                'rewrite' => array( 'slug' => 'punk-tag', 'with_front' => true, 'hierarchical' => true ),
+                'show_admin_column' => true,
+            )
+        );
+    }
+
+?>
